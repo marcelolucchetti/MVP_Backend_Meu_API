7e121741c662828c75ec2bf019636b9029c1278f
marcelolucchetti <marcelolucchetti@gmail.com>
2025-04-10 12:48:21 -0300

    Delete meu_api directory

--- meu_api/model/base.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-# Cria uma classe Base para o instanciamento de novos obetos/tabelas
-Base = declarative_base()
--- meu_api/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.plano import PlanoSchema, PlanoBuscaSchema, PlanoViewSchema, \
-                        ListagemPlanosSchema, PlanoDelSchema, apresenta_planos,\
-                        apresenta_plano, apresenta_planos, PlanoAlteraSchema, PlanoPathSchema
-from schemas.error import ErrorSchema
